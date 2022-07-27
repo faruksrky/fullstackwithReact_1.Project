89917acdafcc752e0a3ce087ffa1abe94fe10af6
faruksrky <faruksrky@gmail.com>
2022-07-27 09:57:20 +0300

    test failed

--- src/test/java/com/example/fullstackspringbootreact/FullStackSpringBootReactApplicationTests.java
+++ src/test/java/com/example/fullstackspringbootreact/FullStackSpringBootReactApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.fullstackspringbootreact;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class FullStackSpringBootReactApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.fail("hi test failed");
     }
 
 }
